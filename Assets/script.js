2c60adcef14d482aeaa74979cd8a138eeb5c07c1
jjobe5989 <s3thjobe@gmail.com>
2023-03-13 19:50:18 -0500

    rewrote code but cannot fix an error I am getting

--- Assets/script.js
+++ Assets/script.js
@@ -1,139 +1,138 @@
 // variable to store and loop through scheduler
-var myDay = [
+let myDay = [
     {
-        id: "0",
-        hour: "09",
-        time: "09",
-        meridiem: "am",
-        reminder: ""
+      id: "0",
+      hour: "09",
+      time: "09",
+      meridiem: "am",
+      reminder: "",
     },
     {
-        id: "1",
-        hour: "10",
-        time: "10",
-        meridiem: "am",
-        reminder: ""
+      id: "1",
+      hour: "10",
+      time: "10",
+      meridiem: "am",
+      reminder: "",
     },
     {
-        id: "2",
-        hour: "11",
-        time: "11",
-        meridiem: "am",
-        reminder: ""
+      id: "2",
+      hour: "11",
+      time: "11",
+      meridiem: "am",
+      reminder: "",
     },
     {
-        id: "3",
-        hour: "12",
-        time: "12",
-        meridiem: "pm",
-        reminder: ""
+      id: "3",
+      hour: "12",
+      time: "12",
+      meridiem: "pm",
+      reminder: "",
     },
     {
-        id: "4",
-        hour: "01",
-        time: "13",
-        meridiem: "pm",
-        reminder: ""
+      id: "4",
+      hour: "01",
+      time: "13",
+      meridiem: "pm",
+      reminder: "",
     },
     {
-        id: "5",
-        hour: "02",
-        time: "14",
-        meridiem: "pm",
-        reminder: ""
+      id: "5",
+      hour: "02",
+      time: "14",
+      meridiem: "pm",
+      reminder: "",
     },
     {
-        id: "6",
-        hour: "03",
-        time: "15",
-        meridiem: "pm",
-        reminder: ""
+      id: "6",
+      hour: "03",
+      time: "15",
+      meridiem: "pm",
+      reminder: "",
     },
     {
-        id: "7",
-        hour: "04",
-        time: "16",
-        meridiem: "pm",
-        reminder: ""
+      id: "7",
+      hour: "04",
+      time: "16",
+      meridiem: "pm",
+      reminder: "",
     },
     {
-        id: "8",
-        hour: "05",
-        time: "17",
-        meridiem: "pm",
-        reminder: ""
+      id: "8",
+      hour: "05",
+      time: "17",
+      meridiem: "pm",
+      reminder: "",
     },
-    
-]
-
-// gets data for the header date
-function getHeaderDate() {
-    var currentHeaderDate = moment().format('dddd, MMMM Do');
+  ];
+  
+  // gets data for the header date
+  function getHeaderDate() {
+    const currentHeaderDate = moment().format("dddd, MMMM Do");
     $("#currentDay").text(currentHeaderDate);
-}
-
-// saves data to localStorage
-function saveReminders() {
+  }
+  
+  // saves data to localStorage
+  function saveReminders() {
     localStorage.setItem("myDay", JSON.stringify(myDay));
-}
-
-// sets any data in localStorage to the view
-function displayReminders() {
-    myDay.forEach(function (_thisHour) {
-        $(`#${_thisHour.id}`).val(_thisHour.reminder);
-    })
-}
-
-// sets any existing localStorage data to the view if it exists
-function init() {
-    var storedDay = JSON.parse(localStorage.getItem("myDay"));
-
+  }
+  
+  // sets any data in localStorage to the view
+  function displayReminders() {
+    myDay.forEach((_thisHour) => {
+      $(`#${_thisHour.id}`).val(_thisHour.reminder);
+    });
+  }
+  
+  // sets any existing localStorage data to the view if it exists
+  function init() {
+    const storedDay = JSON.parse(localStorage.getItem("myDay"));
+  
     if (storedDay) {
-        myDay = storedDay;
+      myDay = storedDay;
     }
-
+  
     saveReminders();
     displayReminders();
-}
-
-// loads header date
-getHeaderDate();
-
-// creates the visuals for the scheduler body
-myDay.forEach(function(thisHour) {
+  }
+  
+  // loads header date
+  getHeaderDate();
+  
+  // creates the visuals for the scheduler body
+  myDay.forEach((thisHour) => {
     // creates timeblocks row
-    var hourRow = $("<form>").attr({
-        "class": "row"
+    const hourRow = $("<form>").attr({
+      class: "row",
     });
     $(".container").append(hourRow);
-
+  
     // creates time field
-    var hourField = $("<div>")
-        .text(`${thisHour.hour}${thisHour.meridiem}`)
-        .attr({
-            "class": "col-md-2 hour"
-    });
-
+    const hourField = $("<div>")
+      .text(`${thisHour.hour}${thisHour.meridiem}`)
+      .attr({
+        class: "col-md-2 hour",
+      });
+  
     // creates schdeduler data
-    var hourPlan = $("<div>")
-        .attr({
-            "class": "col-md-9 description p-0"
-        });
-    var planData = $("<textarea>");
+    const hourPlan = $("<div>")
+      .attr({
+        class: "col-md-9 description p-0",
+      });
+    const planData = $("<textarea>");
     hourPlan.append(planData);
     planData.attr("id", thisHour.id);
     if (thisHour.time < moment().format("HH")) {
-        planData.attr ({
-            "class": "past", 
-        })
+      planData.attr({
+        class: "past",
+      });
     } else if (thisHour.time === moment().format("HH")) {
-        planData.attr({
-            "class": "present"
-        })
+      planData.attr({
+        class: "present",
+      });
     } else if (thisHour.time > moment().format("HH")) {
-        planData.attr({
-            "class": "future"
-        })
+      planData.attr({
+        class: "future",
+      });
     }
 
     // creates save button
@@ -146,16 +145,16 @@ myDay.forEach(function(thisHour) {
     hourRow.append(hourField, hourPlan, savePlan);
 })
 
-// loads any existing localstorage data after components created
-init();
-
 
 // saves data to be used in localStorage
-$(".saveBtn").on("click", function(event) {
+$(".saveBtn").on("click", function (event) {
     event.preventDefault();
-    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
-    myDay[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
-    console.log(saveIndex);
+    var savePlan = $(this).siblings(".description").children(".future").attr("id");
+    myDay[savePlan].reminder = $(this).siblings(".description").find(".future").val();
+    console.log(savePlan);
     saveReminders();
     displayReminders();
-})
+  });
+  
+  // loads any existing localstorage data after components created
+  init();
